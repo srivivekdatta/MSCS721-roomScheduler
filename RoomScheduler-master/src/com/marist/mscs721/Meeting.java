492158aa6568c56b0cbba07725cd388b7386474b
srivivek <srivivekdatta@gmail.com>
2016-02-04 17:20:54 -0500

    Comments added to the meeting class

--- RoomScheduler-master/src/com/marist/mscs721/Meeting.java
+++ RoomScheduler-master/src/com/marist/mscs721/Meeting.java
@@ -29,32 +29,65 @@ public class Meeting {
 	}
 
 	/**
-	 * 
+	 * Formatting the date and time
 	 */
 	public String toString() {
 		return this.getStartTime().toString() + " - " + this.getStopTime() + ": " + getSubject();
 	}
 
+	/**
+	 * Fetches the start time for a selected room
+	 * 
+	 * @return start time for a selected room
+	 */
 	public Timestamp getStartTime() {
 		return startTime;
 	}
 
+	/**
+	 * Sets the start time for a selected room
+	 * 
+	 * @param startTime
+	 *            : Contains the start time for a selected room
+	 */
 	public void setStartTime(Timestamp startTime) {
 		this.startTime = startTime;
 	}
 
+	/**
+	 * Fetches the end time for a selected room
+	 * 
+	 * @return end time for a selected room
+	 */
 	public Timestamp getStopTime() {
 		return stopTime;
 	}
 
+	/**
+	 * Sets the end time for a selected room
+	 * 
+	 * @param stopTime
+	 *            : Contains the end time for a selected room
+	 */
 	public void setStopTime(Timestamp stopTime) {
 		this.stopTime = stopTime;
 	}
 
+	/**
+	 * Fetches the subject for a selected room
+	 * 
+	 * @return subject for a selected room
+	 */
 	public String getSubject() {
 		return subject;
 	}
 
+	/**
+	 * Sets the subject for a selected room
+	 * 
+	 * @param subject
+	 *            : Contains the subject for a selected room
+	 */
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
